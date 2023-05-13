3cdeeae67d686c2614f5d6cac03b146ab9318e19
orangeseeds <shubhechchhakp@gmail.com>
2023-05-13 16:19:55 +0545

    lab3 bst implementation and tests

--- bst_lab3/bst.go
+++ bst_lab3/bst.go
@@ -1,9 +1,12 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type BST struct {
-	Root *Node
+	root *Node
 	size int
 }
 
@@ -15,12 +18,12 @@ type Node struct {
 	Key    int
 }
 
-func NewBST() BST {
-	return BST{}
+func NewBST() *BST {
+	return &BST{}
 }
 
 func (bst *BST) IsEmpty() bool {
-	return bst.Size() == 0
+	return bst.size == 0
 }
 
 func (bst *BST) Size() int {
@@ -34,12 +37,12 @@ func (bst *BST) Add(key int, value string) error {
 	}
 
 	if bst.IsEmpty() {
-		bst.Root = newNode
+		bst.root = newNode
 		bst.size++
 		return nil
 	}
 
-	parentNode := bst.Root
+	parentNode := bst.root
 	for {
 		if newNode.Key < parentNode.Key {
 			if parentNode.Left == nil {
@@ -58,21 +61,28 @@ func (bst *BST) Add(key int, value string) error {
 			}
 			parentNode = parentNode.Right
 		} else {
-			return errors.New("Duplicate key not allowed.")
+			return fmt.Errorf("duplicate key %v not allowed.", key)
 		}
 	}
-
 	return nil
 }
 
-func (bst *BST) Remove() {}
+func (bst *BST) Remove(key int) error {
+
+	if bst.IsEmpty() {
+		return errors.New("BST is empty")
+	}
+
+    bst.root = bst.removeNode(key, bst.root)
+	return nil
+}
 
 func (bst *BST) Search(key int) (string, error) {
 	if bst.IsEmpty() {
 		return "", errors.New("BST is empty")
 	}
 
-	node := bst.Root
+	node := bst.root
 	for node != nil {
 		if key < node.Key {
 			node = node.Left
@@ -82,11 +92,106 @@ func (bst *BST) Search(key int) (string, error) {
 			return node.Value, nil
 		}
 	}
-	return "", errors.New("Key not found")
+	return "", fmt.Errorf("key %v not found", key)
 }
 
-func (bst *BST) Smallest() {}
-func (bst *BST) Largest()  {}
+func (bst *BST) Smallest() (int, string, error) {
+	if bst.IsEmpty() {
+		return 0, "", fmt.Errorf("BST is empty")
+	}
 
-func (bst *BST) InOrderWalk()   {}
-func (bst *BST) PostOrderWalk() {}
+	node := bst.minNode(bst.root)
+	return node.Key, node.Value, nil
+}
+
+func (bst *BST) Largest() (int, string, error) {
+	if bst.IsEmpty() {
+		return 0, "", fmt.Errorf("BST is empty")
+	}
+
+	node := bst.maxNode(bst.root)
+	return node.Key, node.Value, nil
+}
+
+func (bst *BST) InOrderWalk() []int {
+	walk := []int{}
+	inOrder(bst.root, &walk)
+	return walk
+}
+
+func (bst *BST) PreOrderWalk() []int {
+	walk := []int{}
+	preOrder(bst.root, &walk)
+	return walk
+}
+
+func (bst *BST) PostOrderWalk() []int {
+	walk := []int{}
+	postOrder(bst.root, &walk)
+	return walk
+}
+
+func (bst *BST) maxNode(root *Node) *Node {
+	for root.Right != nil {
+		root = root.Right
+	}
+	return root
+}
+func (bst *BST) minNode(root *Node) *Node {
+	for root.Left != nil {
+		root = root.Left
+	}
+	return root
+}
+
+func (bst *BST) removeNode(key int, root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	if key < root.Key {
+		root.Left = bst.removeNode(key, root.Left)
+	} else if key > root.Key {
+		root.Right = bst.removeNode(key, root.Right)
+	} else {
+		if root.Left != nil && root.Right != nil {
+			min := bst.minNode(root.Right)
+			root.Key = min.Key
+			root.Right = bst.removeNode(min.Key, root.Right)
+		} else {
+			if root.Left == nil && root.Right == nil {
+				root = nil
+			} else if root.Left == nil {
+				root = root.Right
+			} else {
+				root = root.Left
+			}
+			bst.size--
+		}
+	}
+	return root
+}
+
+func inOrder(root *Node, walk *[]int) {
+	if root != nil {
+		inOrder(root.Left, walk)
+		*walk = append(*walk, root.Key)
+		inOrder(root.Right, walk)
+	}
+}
+
+func preOrder(root *Node, walk *[]int) {
+	if root != nil {
+		*walk = append(*walk, root.Key)
+		preOrder(root.Left, walk)
+		preOrder(root.Right, walk)
+	}
+}
+
+func postOrder(root *Node, walk *[]int) {
+	if root != nil {
+		postOrder(root.Left, walk)
+		postOrder(root.Right, walk)
+		*walk = append(*walk, root.Key)
+	}
+}
--- bst_lab3/bst_test.go
+++ bst_lab3/bst_test.go
@@ -2,87 +2,168 @@ package main
 
 import (
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
 
-func assertEqual[K comparable](t *testing.T, result K, expected K) {
-	if result != expected {
-		t.Errorf("expected %v but got %v", expected, result)
-	}}
-
-var bst BST
-
-func init() {
-	bst = NewBST()
-	bst.Add(10, "value for 10")
-	bst.Add(52, "value for 52")
-	bst.Add(5, "value for 5")
-	bst.Add(8, "value for 8")
-	bst.Add(1, "value for 1")
-	bst.Add(40, "value for 40")
-	bst.Add(30, "value for 30")
-	bst.Add(45, "Value for 45")
+func setup() *BST {
+	bst := NewBST()
+	_ = bst.Add(10, "Value for 10")
+	_ = bst.Add(52, "Value for 52")
+	_ = bst.Add(5, "Value for 5")
+	_ = bst.Add(8, "Value for 8")
+	_ = bst.Add(1, "Value for 1")
+	_ = bst.Add(40, "Value for 40")
+	_ = bst.Add(30, "Value for 30")
+	_ = bst.Add(45, "Value for 45")
+	return bst
 }
 
 func TestBSTAdd(t *testing.T) {
 	bsTree := NewBST()
 
-	// if bsTree.Size() != 0 {
-	// 	t.Errorf("BST size expected 0 but got %v", bsTree.Size())
-	// }
-    assertEqual(t, 0, bsTree.Size())
+	if bsTree.Size() != 0 {
+		t.Errorf("BST size expected 0 but got %v", bsTree.Size())
+	}
 
 	_ = bsTree.Add(15, "Value is 15")
-	// if bsTree.Size() != 1 {
-	// 	t.Errorf("BST size expected 1 but got %v", bsTree.Size())
-	// }
+	if bsTree.Size() != 1 {
+		t.Errorf("BST size expected 1 but got %v", bsTree.Size())
+	}
 
 	_ = bsTree.Add(10, "Value is 10")
 	if bsTree.Size() != 2 {
 		t.Errorf("BST size expected 2 but got %v", bsTree.Size())
 	}
 
-	res, _ := bsTree.Search(15)
-	if res != "Value is 15" {
-		t.Errorf("Expected 'Value is 15' but got %s", res)
+    result, _ := bsTree.Search(10)
+	if result != "Value is 10" {
+		t.Errorf("Expected 'Value is 10' but got %s", result)
 	}
 
-	res, _ = bsTree.Search(10)
-	if res != "Value is 10" {
-		t.Errorf("Expected 'Value is 10' but got %s", res)
+	result, _ = bsTree.Search(15)
+	if result != "Value is 15" {
+		t.Errorf("Expected 'Value is 15' but got %s", result)
 	}
 }
 
-func TestInOrder(t *testing.T) {}
+func TestInOrder(t *testing.T) {
+	bst := setup()
 
-func TestPostOrder(t *testing.T) {}
+	expected := []int{1, 5, 8, 10, 30, 40, 45, 52}
+	got := bst.InOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
 
-func TestPreOrder(t *testing.T) {}
+	expected = []int{1, 5, 8, 10, 25, 30, 40, 45, 52}
+	_ = bst.Add(25, "Value for 25")
+	got = bst.InOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+
+}
+
+func TestPostOrder(t *testing.T) {
+	bst := setup()
+
+	expected := []int{1, 8, 5, 30, 45, 40, 52, 10}
+	got := bst.PostOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+
+	expected = []int{1, 8, 5, 25, 30, 45, 40, 52, 10}
+	_ = bst.Add(25, "Value for 25")
+	got = bst.PostOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
+
+func TestPreOrder(t *testing.T) {
+	bst := setup()
+
+	expected := []int{10, 5, 1, 8, 52, 40, 30, 45}
+	got := bst.PreOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+
+	expected = []int{10, 5, 1, 8, 52, 40, 30, 25, 45}
+	_ = bst.Add(25, "Value for 25")
+	got = bst.PreOrderWalk()
+	if !slices.Equal(got, expected) {
+		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
 
 func TestSearch(t *testing.T) {
-	expected := "value for 40"
-	got, err := bst.Search(40)
-	if err != nil {
-		t.Error(err)
-	}
+	bst := setup()
+
+	expected := "Value for 40"
+	got, _ := bst.Search(40)
 	if expected != got {
-		t.Errorf("Expected %s but got %s", expected, got)
+		t.Errorf("expected '%s' but got '%s'", expected, got)
+	}
+
+	got, err := bst.Search(90)
+	if err == nil {
+		t.Errorf("expected 'key not found error' but got '%v'", got)
 	}
 
 	expected = "Value for 90"
 	_ = bst.Add(90, "Value for 90")
-	got, err = bst.Search(90)
-	if err != nil {
-		t.Error(err)
-	}
-
+	got, _ = bst.Search(90)
 	if expected != got {
-		t.Errorf("Expected %s but got %s", expected, got)
+		t.Errorf("Expected '%s' but got '%s'", expected, got)
 	}
 }
 
 func TestRemove(t *testing.T) {
+    bst := setup()
+    _ = bst.Remove(40)
+    if bst.Size() != 7 {
+        t.Errorf("expected 7 but got %v", bst.Size())
+    }
+
+    expected := []int{1, 5, 8, 10, 30, 45, 52}
+    got := bst.InOrderWalk()
+    if !slices.Equal(expected, got) {
+		t.Errorf("Expected '%v' but got '%v'", expected, got)
+    }
+
+    expected = []int{10, 5, 1, 8, 52, 30, 45}
+    got = bst.PreOrderWalk()
+    if !slices.Equal(expected, got) {
+		t.Errorf("Expected '%v' but got '%v'", expected, got)
+    }
+
 }
 
-func TestSmallest(t *testing.T) {}
+func TestSmallest(t *testing.T) {
+	bst := setup()
+	_ = bst.Add(6, "Value for 6")
+	_ = bst.Add(4, "Value for 4")
+	_ = bst.Add(0, "Value for 0")
+	_ = bst.Add(32, "Value for 32")
 
-func TestLargest(t *testing.T) {}
+	key, value, _ := bst.Smallest()
+	if key != 0 || value != "Value for 0" {
+		t.Errorf("expected 0, 'Value for 0' but got %v, '%v'", key, value)
+	}
+}
+
+func TestLargest(t *testing.T) {
+	bst := setup()
+	_ = bst.Add(6, "Value for 6")
+	_ = bst.Add(54, "Value for 54")
+	_ = bst.Add(0, "Value for 0")
+	_ = bst.Add(32, "Value for 32")
+
+	key, value, _ := bst.Largest()
+	if key != 54 || value != "Value for 54" {
+		t.Errorf("expected 54, 'Value for 54' but got %v, '%v'", key, value)
+	}
+}
